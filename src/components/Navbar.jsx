2db32b86497700f335cfd0ad0139daf34cee8b3a
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-25 11:11:26 +0600

    responsive home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div className="flex justify-between py-3 container mx-auto space-x-10 w-full md:w-9/12 ">
+    <div className="flex flex-col text-center md:text-left md:flex-row md:justify-between py-3 container mx-auto space-x-0 md:space-x-10 w-full md:w-9/12 ">
       <div className="left flex flex-col my-2">
         <h1 className="text-2xl">
           <span className="font-bold text-blue1">Globe</span>
@@ -11,16 +11,16 @@ const Navbar = () => {
           Company limited
         </small>
       </div>
-      <div className="right my-5">
-        <ul className="flex gap-6">
+      <div className="right my-2 md:my-5">
+        <ul className="flex flex-col md:flex-row gap-3 md:gap-6 justify-start">
           <Link to="/">
-            <li className="text-blue1 font-medium">Home</li>
+            <li className="text-blue1 font-medium cursor-pointer">Home</li>
           </Link>
           <Link to="/services">
-            <li className="text-blue1 font-medium">Services</li>
+            <li className="text-blue1 font-medium pointer">Services</li>
           </Link>
           <Link to="/login">
-            <li className="text-blue1 font-medium">Login</li>
+            <li className="text-blue1 font-medium pointer">Login</li>
           </Link>
         </ul>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,33 @@ import plane from "../images/airplane.png";
 import atom from "../images/atom.png";
 const Home = () => {
   return (
-    <div className="bg-bgBlue h-[820px]">
+    <div className="bg-bgBlue h-full mb-10 ">
       <Navbar />
-      <div className="top flex justify-between mt-24 container mx-auto space-x-10 w-full md:w-9/12 gap-6">
-        <div className="left w-1/2">
-          <div className="plane mb-14 left-0 duration-300 ease-in-out hover:translate-x-3/4">
+      <div className="top flex flex-col lg:flex-row justify-between mt-10 md:mt-24 container mx-auto space-x-10 w-full md:w-9/12 gap-6">
+        <div className="left w-full p-2 lg:w-1/2">
+          <div className="plane mb-7 md:mb-14 left-0 duration-300 ease-in-out hover:translate-x-3/4">
             <img className="w-24 h-16" src={plane} alt="" />
           </div>
-          <h1 className="text-6xl font-bold text-blue1 mb-6">Welcome to </h1>
+          <h1 className="text-5xl md:text-6xl font-bold text-blue1 mb-6">
+            Welcome to{" "}
+          </h1>
           <div className="detail">
-            <h1 className="text-6xl font-bold text-gold mb-2">Globetech</h1>
+            <h1 className="text-5xl md:text-6xl font-bold text-gold mb-2">
+              Globetech
+            </h1>
             <p className="text-blue2 text-lg font-normal text-justify">
               We are committed to deliver <span>best IT services</span>. Our
               consultants have experience in working with clients. We have
               extensive experience in the software application space and also
               offer a broad range and depth of technology.
             </p>
-            <div className="detail-button mt-4 space-x-8">
+            <div className="detail-button mt-4 gap-5 flex flex-grow">
               <button className="custom-button">Support us</button>
               <button className="custom-button">Our Mission</button>
             </div>
           </div>
         </div>
-        <div className="right w-1/2 duration-300 delay-200 ease-linear hover:rotate-45">
+        <div className="right w-1/2 duration-300 delay-200 ease-linear hover:rotate-45 hidden lg:block">
           <img className="w-full h-full" src={atom} alt="" />
         </div>
       </div>
