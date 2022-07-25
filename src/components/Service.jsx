562931336e626cf40e2f819775372a45f24825ae
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-25 12:36:03 +0600

    service component update

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-
-const Service = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import plane from "../images/airplane.png"
+const Service = ({title}) => {
+  return (
+    <div className="cart bg-white w-full md:w-[45%] lg:w-[30%] xl:w-[31%] rounded-2xl p-5">
+      <img className="w-full " src={plane} alt="" />
+      <h1 className="text-center mt-7 mb-3 text-xl tracking-widest text-blue1">
+        {title}
+      </h1>
+      <p className="text-[12px] text-justify my-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, porro?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, porro?
+      </p>
+      <div className="text-center mb-2">
+        <button className="uppercase tracking-widest text-white font-medium text-lg bg-blue1 my-2 px-12 py-2 border rounded-3xl cursor-pointer">
+          More
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import Service from "../components/Service";
 import plane from "../images/airplane.png";
 const Services = () => {
   return (
@@ -7,13 +8,22 @@ const Services = () => {
       <div className="planeBox w-full left-0 duration-1000 ease-in-out hover:translate-x-full container mx-auto space-x-10  md:w-9/12 gap-6 mt-10">
         <img className="w-24 h-16" src={plane} alt="" />
       </div>
-      <div className="services container mx-auto w-full md:w-9/12 mt-12">
+      <div className="services container mx-auto w-full md:w-9/12 my-12">
         <div className="top text-center mb-5">
-          <h1 className="text-white font-medium text-3xl uppercase tracking-widest">Services</h1>
-          <h1 className="text-white font-semibold text-4xl tracking-widest my-5">We provides services to our clients</h1>
+          <h1 className="text-white font-medium text-3xl uppercase tracking-widest">
+            Services
+          </h1>
+          <h1 className="text-white font-semibold text-4xl tracking-widest my-5">
+            We provides services to our clients
+          </h1>
         </div>
-        <div className="serviceCart">
-            
+        <div className="serviceCart flex flex-col md:flex-row flex-wrap gap-8">
+          <Service title="Software Solution"/>
+          <Service title=""/>
+          <Service title=""/>
+          <Service title=""/>
+          <Service title=""/>
+          <Service title=""/>
         </div>
       </div>
     </div>
